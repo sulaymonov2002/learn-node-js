03f39557c16534d900389858bd4705ad4f5ba3ae
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-01-29 17:40:32 +0500

    feat: add new codes first node.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const _ = require("underscore");
+const mongoose = require("mongoose");
+
+// Core module
+// fayl yoki papka
+// node_modules
+
+const result = _.contains([3, 5, 9], 5);
+
+console.log(result);
