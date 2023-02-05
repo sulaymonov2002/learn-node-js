2b6c21a4e5ebb43634e579cc9d24dcbafa00c866
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-02-05 23:18:03 +0500

    feat: add new node.js backend

--- index.js
+++ index.js
@@ -3,89 +3,32 @@ const Joi = require("joi");
 const app = express();
 app.use(express.json());
 
-const books = [
-  { id: 1, name: "rich dad poor dad" },
-  { id: 2, name: "good to great" },
-  { id: 3, name: "rework" },
+const categories = [
+  { id: 1, name: "Dasturlash" },
+  { id: 2, name: "Ma'lumot omborlari" },
+  { id: 3, name: "Kompyuter tarmoqlari" },
 ];
 
-app.get("/", (req, res) => {
-  res.send("Salom");
+app.get("/api/categories", (req, res) => {
+  res.send(categories);
 });
 
-app.get("/api/books", (req, res) => {
-  res.send(books);
-});
-
-app.post("/api/books", (req, res) => {
-  const { error } = validateBook(req.body);
-
+app.post("/api/categories", (req, res) => {
+  const { error } = validateCategory(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
 
-  const book = {
-    id: books.length + 1,
+  const category = {
+    id: categories.length + 1,
     name: req.body.name,
   };
-  books.push(book);
-  res.status(201).send(book);
+  categories.push(category);
+  res.status(201).send(category);
 });
 
-app.get("/api/books/:id", (req, res) => {
-  const book = books.find((b) => b.id === parseInt(req.params.id));
-
-  if (!book)
-    return res.status(404).send("Berilgan IDga teng bo'lgan kitob topilmadi");
-
-  res.send(book);
-});
-
-app.put("/api/books/:id", (req, res) => {
-  // kitobni  bazadan izlab topish
-  // agarada kitob mavjud bo'lmasa, 404 qaytarish
-  const book = books.find((b) => b.id === parseInt(req.params.id));
-  if (!book)
-    return res.status(404).send("Berilgan IDga teng bo'lgan kitob topilmadi");
-
-  // agar kitob topilsa, so'rovni validatsiya qilish
-  // agar so'rov validatsiyada o'tmasa, 400 qaytarish
-
-  const { error } = validateBook(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  // kitobni yangilash
-  book.name = req.body.name;
-  // yangilangan kitobni qaytarish
-  res.send(book);
-});
-
-app.delete("/api/books/:id", (req, res) => {
-  // kitobni idsi bo'yicha izlab topamiz
-  // agar topilmasa 404 qaytaramiz
-  const book = books.find((b) => b.id === parseInt(req.params.id));
-  if (!book)
-    return res.status(404).send("Berilgan IDga teng bo'lgan kitob topilmadi ");
-
-  // topilsa uni o'chirib tashlaymiz
-  const bookIndex = books.indexOf(book);
-  books.splice(bookIndex, 1);
-  // topilgan kitobni qaytarib beramiz
-  res.send(book);
-});
-
-function validateBook(book) {
-  const bookSchema = {
-    name: Joi.string().required().min(3),
-  };
-
-  return Joi.validate(book, bookSchema);
-}
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`${port}chi portni eshitishni boshladim... `);
+app.get("/api/categories:id", (req, res) => {
+  const category = categories.find((c) => c.id === parseInt(req.params.id));
+  if (!category)
+    return res.status(404).send("Berilgan IDga teng bo'lgan toifa topilmadi");
 });
