b46f2246ee8bdd49922be79c586b48f72e903ef5
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-02-05 17:22:56 +0500

    feat: add validation and Joi npm package

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
-const _ = require("underscore");
-const mongoose = require("mongoose");
+const express = require("express");
+const Joi = require("joi");
+const app = express();
+app.use(express.json());
 
-// Core module
-// fayl yoki papka
-// node_modules
+const books = [
+  { id: 1, name: "rich dad poor dad" },
+  { id: 2, name: "good to great" },
+  { id: 3, name: "rework" },
+];
 
-const result = _.contains([3, 5, 9], 5);
+app.get("/", (req, res) => {
+  res.send("Salom");
+});
 
-console.log(result);
+app.get("/api/books", (req, res) => {
+  res.send(books);
+});
+
+app.post("/api/books", (req, res) => {
+  const bookSchema = {
+    name: Joi.string().required().min(3),
+  };
+
+  const result = Joi.validate(req.body, bookSchema);
+
+  const book = {
+    id: books.length + 1,
+    name: req.body.name,
+  };
+  books.push(book);
+  res.status(201).send(book);
+});
+
+app.get("/api/books/:id", (req, res) => {
+  const book = books.find((b) => b.id === parseInt(req.params.id));
+  if (!book) res.status(404).send("Berilgan IDga teng bo'lgan kitob topilmadi");
+
+  res.send(books);
+});
+
+app.get("/api/articles/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`${port}chi portni eshitishni boshladim... `);
+});
