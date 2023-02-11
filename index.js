76cc161b52415fb1c5a8939d3c725302edda2be8
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-02-12 00:23:37 +0500

    feat: add new function codes

--- index.js
+++ index.js
@@ -3,71 +3,39 @@ const Joi = require("joi");
 const app = express();
 app.use(express.json());
 
-const categories = [
-  { id: 1, name: "Dasturlash" },
-  { id: 2, name: "Ma'lumot omborlari" },
-  { id: 3, name: "Kompyuter tarmoqlari" },
+app.use(function (req, res, next) {
+  console.log("log yozish...");
+  next();
+});
+
+app.use(function (req, res, next) {
+  console.log("Authentifikatsiya qilish...");
+  next();
+});
+
+const books = [
+  { id: 1, name: "rich dad poor dad" },
+  { id: 2, name: "good for great" },
+  { id: 3, name: "rework" },
 ];
 
-app.get("/api/categories", (req, res) => {
-  res.send(categories);
+app.get("/", (req, res) => {
+  res.send("Salom");
 });
 
-app.post("/api/categories", (req, res) => {
-  const { error } = validateCategory(req.body);
+app.get("/api/books", (req, res) => {
+  res.send(books);
+});
+
+app.post("/api/books", (req, res) => {
+  res.send(books);
+});
+
+app.post("/api/books", (req, res) => {
+  const { error } = validateBook(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
 
-  const category = {
-    id: categories.length + 1,
-    name: req.body.name,
-  };
-  categories.push(category);
-  res.status(201).send(category);
-});
-
-app.get("/api/categories:id", (req, res) => {
-  const category = categories.find((c) => c.id === parseInt(req.params.id));
-  if (!category)
-    return res.status(404).send("Berilgan IDga teng bo'lgan toifa topilmadi");
-
-  res.send(category);
-});
-
-app.put("/api/categories:id", (req, res) => {
-  const category = categories.find((c) => c.id === parseInt(req.params.id));
-  if (!category)
-    return res.status(404).send("Berilgan IDga teng bo'lgan toifa topilmadi");
-
-  const { error } = validateCategory(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  category.name = req.body.name;
-  res.send(category);
-});
-
-app.delete("/api/categories/:id", (req, res) => {
-  const category = categories.find((c) => c.id === parseInt(req.params.id));
-  if (!category)
-    return res.status(404).send("Berilgan IDga teng bo'lgan toifa topilmadi");
-
-  const categoryIndex = categories.indexOf(category);
-  categories.splice(categoryIndex, 1);
-  res.send(category);
-});
-
-function validateCategory(category) {
-  const schema = {
-    name: Joi.string().required().min(3),
-  };
-  return Joi.validate(category, schema);
-}
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`${port}chi portni eshitishni boshladim...`);
+  const book = [];
 });
